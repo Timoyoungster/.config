778fd439f482c3fde89e1f3ebb100764d8f44904
Timoyoungster <timo.proemer04@gmail.com>
2025-05-08 14:14:34 +0200

    feat: setup nchat

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 ln -s ~/.config/zsh/.zshrc ~/.zshrc
 ln -s ~/.config/git/.gitconfig ~/.gitconfig 
+mv nchat nchat.old
+echo "To setup nchat, please go through the setup process first. After that, close it and move the contents of ./nchat.old back into ./nchat"
