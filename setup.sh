12bcddd7b6df44fc50ae2f454955383ac77b926c
Timoyoungster <timo.proemer04@gmail.com>
2025-05-08 14:17:18 +0200

    fix: setup gitconfig

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 ln -s ~/.config/zsh/.zshrc ~/.zshrc
+
+mkdir git
+touch git/.gitconfig
 ln -s ~/.config/git/.gitconfig ~/.gitconfig 
+echo "Please setup your .gitconfig manually"
+
 mv nchat nchat.old
 echo "To setup nchat, please go through the setup process first. After that, close it and move the contents of ./nchat.old back into ./nchat"
