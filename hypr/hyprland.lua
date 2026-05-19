914ec4f5773e0cb531e7defd02ae7954b8eb3955
Timoyoungster <timo.proemer04@gmail.com>
2026-05-19 17:30:29 +0200

    chores: hyprshot uses grim under the hood => permission

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -88,7 +88,7 @@ hl.config({
   },
 })
 
--- hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
+hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
 hl.permission("/usr/(bin|local/bin)/hyprshot", "screencopy", "allow")
 hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
 hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
